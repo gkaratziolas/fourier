be525d0d7cfb5913c7dde9b6f7bc3ecab50e7cc6
Giorgos Karatziolas <giorgos.karatziolas@garrison.com>
2018-02-27 16:59:15 +0000

    Add imaginary component to fourier plot

--- ft.py
+++ ft.py
@@ -6,7 +6,7 @@ from scipy import signal
 TMAX  = 6*np.pi
 TSTEP = 0.01
 WMAX  = 10
-WSTEP = 0.01
+WSTEP = 0.02
 
 def complex_to_array(c):
 	"""converts complex numbers to (2,1) np array of reals"""
@@ -17,8 +17,9 @@ def complex_to_array(c):
 
 def g(t):
 	"""Returns a predefined function of time for a given time vector"""
-	#return signal.sawtooth(3*t, width=0.5)+1
-	return np.cos(4*t) + np.cos(3*t) + 1
+	return signal.sawtooth(3*t, width=0.5)
+	#return signal.square(t)
+	#return np.cos(4*t) + np.cos(3*t) + 1
 	#return np.cosh(t)
 	#return np.cos(t)*np.sin(10*t)
 
@@ -41,15 +42,22 @@ ax3 = fig.add_subplot(224)
 
 ax2.plot(t,g(t))
 
-data = [g(t)*np.e**(1j*0.0*t), centre_of_mass(g(t)*np.e**(1j*0.0*t)), np.array([[],[]])]
+data = [g(t)*np.e**(1j*0.0*t),
+        centre_of_mass(g(t)*np.e**(1j*0.0*t)),
+        np.array([[],[]]),
+        np.array([[],[]])]
 
 lines = []
 line, = ax1.plot(1)
 lines.append(line)
 line, = ax1.plot(1, "ro")
 lines.append(line)
-line, = ax3.plot([],[])
+
+line, = ax3.plot([],[], label='real')
 lines.append(line)
+line, = ax3.plot([],[], "g", label='imag')
+lines.append(line)
+ax3.legend()
 
 ax1.set_rmax(max(g(t))*1.1)
 ax1.set_rticks([])
@@ -63,12 +71,15 @@ def animate(w):
 	com = centre_of_mass(data[0])
 	data[1] = [com]
 	
-	ft = np.array([[w], [com.real]])
-	data[2] = np.append(data[2], ft, axis=1)
+	ftr = np.array([[w], [com.real]])
+	fti = np.array([[w], [com.imag]])
+	data[2] = np.append(data[2], ftr, axis=1)
+	data[3] = np.append(data[3], fti, axis=1)
 
 	lines[0].set_data(complex_to_array(data[0]))
 	lines[1].set_data(complex_to_array(data[1]))
 	lines[2].set_data(data[2])
+	lines[3].set_data(data[3])
 
 	return lines
 
